bf2f0ce9237d11971f4fd01aa482599e92620007
GabrielFierro <gabriel.fierro@est.fi.uncoma.edu.ar>
2021-07-24 20:05:37 -0300

    add: add the theme for the project

---
+++ src/Theme.js
@@ -0,0 +1,28 @@
+// theme.js
+import { extendTheme } from "@chakra-ui/react";
+import { mode } from "@chakra-ui/theme-tools";
+// 2. Call `extendTheme` and pass your custom values
+const theme = extendTheme({
+	styles: {
+		global: (props) => ({
+			body: {
+				margin: 0,
+				padding: 0,
+				color: "#0BC5EA",
+				bg: mode("#1A202C")(props),
+				lineHeight: "base",
+			},
+		}),
+	},
+	colors: {
+		brand: {
+			100: "#ED64A6",
+		},
+	},
+	fonts: {
+		body: "Montserrat",
+		fontWeight: 700,
+	},
+});
+
+export default theme;
