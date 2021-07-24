e74a80cc9c4964d729167b2c3d107769e16bebf7
GabrielFierro <gabriel.fierro@est.fi.uncoma.edu.ar>
2021-07-24 20:06:42 -0300

    delete the background states, add a activeState for every shape, fix the properties, add a title

--- src/App.js
+++ src/App.js
@@ -1,48 +1,61 @@
 import { useState } from "react";
-import { Box, Center, Circle, ChakraProvider, Grid } from "@chakra-ui/react";
+import { Box, Center, Grid, Text } from "@chakra-ui/react";
+import "./App.css";
 
 function App() {
-	const [background0, setBackground0] = useState("#171923");
-	const [background1, setBackground1] = useState("#171923");
-	const [background2, setBackground2] = useState("#171923");
-	const [background3, setBackground3] = useState("#171923");
-	const [background4, setBackground4] = useState("#171923");
-	const [background5, setBackground5] = useState("#171923");
-	const [background6, setBackground6] = useState("#171923");
-	const [background7, setBackground7] = useState("#171923");
-	const [background8, setBackground8] = useState("#171923");
+	const [background0, setBackground0] = useState("primary");
 	const [counter, setCounter] = useState(0);
+	const [activeCircle0, setActiveCircle0] = useState(false);
+	const [activeCircle1, setActiveCircle1] = useState(false);
+	const [activeCircle2, setActiveCircle2] = useState(false);
+	const [activeCircle3, setActiveCircle3] = useState(false);
+	const [activeCircle4, setActiveCircle4] = useState(false);
+	const [activeCross0, setActiveCross0] = useState(false);
+	const [activeCross1, setActiveCross1] = useState(false);
+	const [activeCross2, setActiveCross2] = useState(false);
+	const [activeCross3, setActiveCross3] = useState(false);
 
-	function handleBoxColor(id) {
-		if (id % 2 === 0) {
+	function handlesShapes() {
+		if (counter % 2 === 0) {
 			// if the id of the box is even then call to the handleBoxColor function
-			handleBoxColorEven(id);
+			//handleBoxColorEven(id);
+			handleCircle(counter);
 		} else {
 			// if the id of the box is odd then call to the handleBoxColor2 function
-			handleBoxColorOdd(id);
+			handleCross(counter);
 		}
 	}
 
-	function handleBoxColorEven(id) {
-		switch (id) {
+	function drawACircle() {
+		// function that return a div with the class circle that display that shape
+		return <div className="circle"></div>;
+	}
+
+	function drawACross() {
+		// function that return a div with the id cross that display that shape
+		return <div id="cross"></div>;
+	}
+
+	function handleCircle(counter) {
+		switch (counter) {
 			case 0:
-				setBackground0("#0BC5EA");
+				setActiveCircle0(true);
 				setCounter(counter + 1);
 				break;
 			case 2:
-				setBackground2("#0BC5EA");
+				setActiveCircle1(true);
 				setCounter(counter + 1);
 				break;
 			case 4:
-				setBackground4("#0BC5EA");
+				setActiveCircle2(true);
 				setCounter(counter + 1);
 				break;
 			case 6:
-				setBackground6("#0BC5EA");
+				setActiveCircle3(true);
 				setCounter(counter + 1);
 				break;
 			case 8:
-				setBackground8("#0BC5EA");
+				setActiveCircle4(true);
 				setCounter(counter + 1);
 				break;
 			default:
@@ -50,38 +63,42 @@ function App() {
 		}
 	}
 
-	function handleBoxColorOdd(id) {
-		switch (id) {
+	function handleCross(counter) {
+		switch (counter) {
 			case 1:
-				setBackground1("#ED64A6");
+				setActiveCross0(true);
 				setCounter(counter + 1);
 				break;
 			case 3:
-				setBackground3("#ED64A6");
+				setActiveCross1(true);
 				setCounter(counter + 1);
 				break;
 			case 5:
-				setBackground5("#ED64A6");
+				setActiveCross2(true);
 				setCounter(counter + 1);
 				break;
 			case 7:
-				setBackground7("#ED64A6");
+				setActiveCross3(true);
 				setCounter(counter + 1);
 				break;
 			default:
 				break;
 		}
-		setCounter(counter + 1);
 	}
 
 	return (
-		<ChakraProvider>
-			<Center h="100vh" bg="#171923">
+		<Grid w="100%" h="100%" bg="primary">
+			<Center>
+				<Text color="primary" fontSize="98px">
+					TA-TE-TI
+				</Text>
+			</Center>
+			<Center h="100%" bg="primary">
 				<Grid
 					w="500px"
 					h="500px"
 					border="7px"
-					borderColor="gray.300"
+					borderColor="#ED64A6"
 					templateColumns="repeat(3, 1fr)"
 				>
 					<Box
@@ -89,85 +106,103 @@ function App() {
 						h="100%"
 						id={0}
 						bg={background0}
-						borderColor="purple.400"
+						borderColor="#00B5D8"
 						borderWidth="3px"
-						onClick={() => handleBoxColor(0)}
-					/>
+						onClick={() => handlesShapes()}
+					>
+						{activeCircle0 ? drawACircle() : null}
+					</Box>
 					<Box
 						w="100%"
 						h="100%"
 						id={1}
-						bg={background1}
-						borderColor="purple.400"
+						bg={background0}
+						borderColor="#00B5D8"
 						borderWidth="3px"
-						onClick={() => handleBoxColor(1)}
-					/>
+						onClick={() => handlesShapes()}
+					>
+						{activeCross0 ? drawACross() : null}
+					</Box>
 					<Box
 						w="100%"
 						h="100%"
 						id={2}
-						bg={background2}
-						borderColor="purple.400"
+						bg={background0}
+						borderColor="#00B5D8"
 						borderWidth="3px"
-						onClick={() => handleBoxColor(2)}
-					/>
+						onClick={() => handlesShapes()}
+					>
+						{activeCircle1 ? drawACircle() : null}
+					</Box>
 					<Box
 						w="100%"
 						h="100%"
 						id={3}
-						bg={background3}
-						borderColor="purple.400"
+						bg={background0}
+						borderColor="#00B5D8"
 						borderWidth="3px"
-						onClick={() => handleBoxColor(3)}
-					/>
+						onClick={() => handlesShapes()}
+					>
+						{activeCross1 ? drawACross() : null}
+					</Box>
 					<Box
 						w="100%"
 						h="100%"
 						id={4}
-						bg={background4}
-						borderColor="purple.400"
+						bg={background0}
+						borderColor="#00B5D8"
 						borderWidth="3px"
-						onClick={() => handleBoxColor(4)}
-					/>
+						onClick={() => handlesShapes()}
+					>
+						{activeCircle2 ? drawACircle() : null}
+					</Box>
 					<Box
 						w="100%"
 						h="100%"
 						id={5}
-						bg={background5}
-						borderColor="purple.400"
+						bg={background0}
+						borderColor="#00B5D8"
 						borderWidth="3px"
-						onClick={() => handleBoxColor(5)}
-					/>
+						onClick={() => handlesShapes()}
+					>
+						{activeCross2 ? drawACross() : null}
+					</Box>
 					<Box
 						w="100%"
 						h="100%"
 						id={6}
-						bg={background6}
-						borderColor="purple.400"
+						bg={background0}
+						borderColor="#00B5D8"
 						borderWidth="3px"
-						onClick={() => handleBoxColor(6)}
-					/>
+						onClick={() => handlesShapes()}
+					>
+						{activeCircle3 ? drawACircle() : null}
+					</Box>
 					<Box
 						w="100%"
 						h="100%"
 						id={7}
-						bg={background7}
-						borderColor="purple.400"
+						bg={background0}
+						borderColor="#00B5D8"
 						borderWidth="3px"
-						onClick={() => handleBoxColor(7)}
-					/>
+						onClick={() => handlesShapes()}
+					>
+						{activeCross3 ? drawACross() : null}
+					</Box>
 					<Box
 						w="100%"
 						h="100%"
 						id={8}
-						bg={background8}
-						borderColor="purple.400"
+						bg={background0}
+						borderColor="#00B5D8"
 						borderWidth="3px"
-						onClick={() => handleBoxColor(8)}
-					/>
+						onClick={() => handlesShapes()}
+					>
+						{activeCircle4 ? drawACircle() : null}
+					</Box>
 				</Grid>
 			</Center>
-		</ChakraProvider>
+		</Grid>
 	);
 }
 
